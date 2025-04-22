9cf80eb5b885a1e662b707a6a5d96fe781bbe79c
abir-jaouani1 <abir.jaouani@esi.ac.ma>
2025-04-22 22:48:48 +0200

    ajouter la configuration du plugin jacoco pour la couverture des tests

---
+++ src/test/java/com/example/one/service/CustomerServiceTest.java
@@ -0,0 +1,12 @@
+package com.example.one.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerServiceTest {
+
+    @Test
+    void addCustomer() {
+    }
+}
