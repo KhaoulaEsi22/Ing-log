d3de4cf4925958bd7e76fe27aa6216b7acbf4877
KHAOULa <khaoula.messas@esi.ac.ma>
2025-04-22 16:42:47 +0200

    Initial commit

---
+++ src/test/java/com/example/one/service/ProductServiceTest.java
@@ -0,0 +1,16 @@
+package com.example.one.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class ProductServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void addProduct() {
+    }
+}
